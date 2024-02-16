f76fc60c01cccce6966037b196f9a0b26ddfd66d
4455bk <rundsxe@gmail.com>
2024-02-17 06:42:13 +0800

    msm8996-common: Change clang version to r450784e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,6 +44,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sle
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
+TARGET_KERNEL_CLANG_VERSION := r450784e
 TARGET_KERNEL_SOURCE := kernel/zuk/msm8996
 TARGET_KERNEL_BUILD_HOST := Ubuntu
 TARGET_KERNEL_BUILD_USER := "Runds"
